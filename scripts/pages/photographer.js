4995f9909a1f545f62968b12f5dc2a27d03ffb64
stephane.malho <stepahen.malho@gmail.com>
2023-04-03 13:52:30 +0200

    🚧 F05: photographer.js > add a new method to get the current image index

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,10 +9,14 @@ async function getPhotographerData() {
     (photographer) => photographer.id == id
   );
 
+  console.log(photographer);
+
   const responseMedia = await fetch("../data/photographers.json");
   const dataMedia = await responseMedia.json();
   const media = dataMedia.media.filter((media) => media.photographerId == id);
   console.log(media);
+  // stocker dans une variable tout les médias du photographe pour les trier
+  
 
 
   function getLikes(photographerId) {
@@ -53,14 +57,14 @@ async function getPhotographerData() {
   }€/jour</p>
   `;
 
-  // Créer la liste des photos du photographe en créeant un élément html div
+  //médias container
   const mediaList = document.querySelector(".photograph-media");
 
   // Ajouter la select box pour trier les médias
-  const sortSelect = document.getElementById("sortMedias")
+  const sortSelect = document.getElementById("sortMedias");
   // placer sortSelect dans le h1 de la page
   sortSelect.setAttribute("aria-label", "Trier les médias");
-  
+
   // Ajouter l'écouteur d'événement pour trier les médias en fonction de la valeur de la select box
   sortSelect.addEventListener("change", (e) => {
     const value = e.target.value;
@@ -78,154 +82,179 @@ async function getPhotographerData() {
         media.sort((a, b) => a.title.localeCompare(b.title));
         break;
       default:
-        mediaList.appendChild(mediaCard);
+        mediaList.innerHTML = "";
         break;
     }
-    
-    // Supprimer tous les médias de la page
 
-  media.forEach((photographer) => {
-    let mediaElement;
-
-    if (photographer.hasOwnProperty("video")) {
-      // Si le média contient une vidéo, créer une balise vidéo et y ajouter l'URL de la vidéo
-      mediaElement = document.createElement("video");
-      mediaElement.setAttribute("aria-label", "video de " + photographer.title);
-      mediaElement.src = `assets/videos/${photographer.video}`;
-      mediaElement.controls = true;
-    } else {
-      // Si le média ne contient pas de vidéo, créer une balise img et y ajouter l'URL de l'image
-      mediaElement = document.createElement("img");
-      mediaElement.src = `assets/photos/${photographer.image}`;
-      mediaElement.alt = photographer.title;
-    }
-
-    // Créer un élément div pour chaque média et y ajouter le mediaElement et les autres éléments
-    const mediaCard = document.createElement("div");
-    mediaCard.classList.add("cards");
-    mediaCard.addEventListener("click", displaySlider);
-    mediaCard.addEventListener("keyup", (e) => {
-      if (e.key === "Enter") {
-        displaySlider(mediaCard.id);
+    // Afficher les médias triés
+    media.forEach((photographer) => {
+      let mediaElement;
+      if (photographer.hasOwnProperty("video")) {
+        // Si le média contient une vidéo, créer une balise vidéo et y ajouter l'URL de la vidéo
+        mediaElement = document.createElement("video");
+        mediaElement.setAttribute(
+          "aria-label",
+          "video de " + photographer.title
+        );
+        mediaElement.src = `assets/videos/${photographer.video}`;
+        mediaElement.controls = true;
+      } else {
+        // Si le média ne contient pas de vidéo, créer une balise img et y ajouter l'URL de l'image
+        mediaElement = document.createElement("img");
+        mediaElement.src = `assets/photos/${photographer.image}`;
+        mediaElement.alt = photographer.title;
       }
-    });
-    mediaCard.id = photographer.id;
-    mediaCard.setAttribute("tabindex", "0");
-    mediaCard.innerHTML = `
+
+      // Créer un élément div pour chaque média et y ajouter le mediaElement et les autres éléments
+      const mediaCard = document.createElement("div");
+      mediaCard.classList.add("cards");
+      mediaCard.addEventListener("click", () => {
+          
+        const cards = document.querySelectorAll('.cards');
+        cards.forEach((card, index) => {
+          if (card.id == photographer.id) {
+            console.log(index);
+            displaySlider();
+          }
+        });
+      });
+      mediaCard.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+          // si le bouton like est focus on ne lance pas la fonction displaySlider
+          if (e.target.classList.contains("likes")) {
+            return;
+          } else {
+            displaySlider(mediaCard.id);
+          }
+        }
+      });
+      mediaCard.id = photographer.id;
+      mediaCard.setAttribute("tabindex", "0");
+      mediaCard.innerHTML = `
     <div class="descriptionBox">
     <h2>${photographer.title}</h2>
     <button class="likes"><p>${photographer.likes}</p><i class="fas fa-heart"></i></button>
     </div>
     `;
-    mediaCard.appendChild(mediaElement);
-    
-    // Ajouter un like au click sur le bouton avec la classe likes et ne pas jouer l'ébènement click sur la carte du média
-    const likeButton = mediaCard.querySelector(".likes");
-    likeButton.addEventListener("click", (e) => {
-      e.stopPropagation();
-      photographer.likes++;
-      // si le like à déja été cliquer on ne l'ajoute pas
-      if (likeButton.classList.contains("liked")) {
-        return;
-      }
-      likeButton.classList.add("liked");
-      
-      // ajouter un like au média dans le tableau media et dans le fichier json media.json
-      media.forEach((media) => {
-        if (media.id == photographer.id) {
-          likeButton.innerHTML = `
+      mediaCard.appendChild(mediaElement);
+
+      // Ajouter un like au click sur le bouton avec la classe likes et ne pas jouer l'ébènement click sur la carte du média
+      const likeButton = mediaCard.querySelector(".likes");
+      likeButton.addEventListener("click", (e) => {
+        e.stopPropagation();
+        photographer.likes++;
+        // si le like à déja été cliquer on ne l'ajoute pas
+        if (likeButton.classList.contains("liked")) {
+          return;
+        }
+
+        likeButton.classList.add("liked");
+
+        // ajouter un like au média dans le tableau media et dans le fichier json media.json
+        media.forEach((media) => {
+          if (media.id == photographer.id) {
+            likeButton.innerHTML = `
           <p>${media.likes}</p>
           <i class="fas fa-heart red"></i>
           `;
 
-          console.log(media.likes);
-          const likes = document.querySelector(".banner span");
-          likes.innerHTML = `
+            console.log(media.likes);
+            const likes = document.querySelector(".banner span");
+            likes.innerHTML = `
           ${getLikes(id)}
           <i class="fas fa-heart" aria-label=" nombres de likes">  </i>
           `;
+          }
+        });
+      });
+
+      // Ajouter la carte du média à la liste des médias
+      mediaList.appendChild(mediaCard);
+      
+      
+      const carrousel = document.querySelector(".carrousel-media");
+      carrousel.innerHTML = "";
+      media.forEach((media) => {
+        let mediaElement;
+        if (media.hasOwnProperty("video")) {
+          // Si le média contient une vidéo, créer une balise vidéo et y ajouter l'URL de la vidéo
+          mediaElement = document.createElement("video");
+          mediaElement.setAttribute("aria-label", "video de " + media.title);
+          mediaElement.classList.add("mediaElement");
+          mediaElement.src = `assets/videos/${media.video}`;
+          mediaElement.controls = true;
+        } else {
+          // Si le média ne contient pas de vidéo, créer une balise img et y ajouter l'URL de l'image
+          mediaElement = document.createElement("img");
+          mediaElement.classList.add("mediaElement");
+          mediaElement.setAttribute("aria-label", "image de " + media.title);
+          mediaElement.setAttribute("tabindex", "0");
+          mediaElement.src = `assets/photos/${media.image}`;
+          mediaElement.alt = media.title;
+        }
+        const mediaCard = document.createElement("div");
+        mediaCard.classList.add("images");
+        mediaCard.id = media.id;
+
+        const imageContainer = document.createElement("div");
+        imageContainer.classList.add("imageContainer");
+
+        mediaCard.appendChild(imageContainer);
+        carrousel.appendChild(mediaCard);
+
+        imageContainer.appendChild(mediaElement);
+
+        //ajouter le titre du média à la carte du média
+        const mediaTitle = document.createElement("h2");
+        mediaTitle.innerHTML = media.title;
+        mediaTitle.classList.add("mediaTitle");
+        mediaCard.appendChild(mediaTitle);
+      });
+    
+      // au click sur entrer ouvrir le carrousel
+      document.getElementById(mediaCard.id).addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+          // afficher le tableau de média
+          const carrousel = document.querySelector(".carrousel-media");
+          carrousel.innerHTML = "";
+          // si il existe la class liked on return pour la pas jouer displaySlider
+          media.forEach((media) => {
+            let mediaElement;
+            if (media.hasOwnProperty("video")) {
+              // Si le média contient une vidéo, créer une balise vidéo et y ajouter l'URL de la vidéo
+              mediaElement = document.createElement("video");
+              mediaElement.setAttribute(
+                "aria-label",
+                "video de " + media.title
+              );
+              mediaElement.classList.add("mediaElement");
+              // add a size to the video to avoid the video to be bigger than the screen
+              mediaElement.width = 720;
+              mediaElement.src = `assets/videos/${media.video}`;
+              mediaElement.controls = true;
+            } else {
+              // Si le média ne contient pas de vidéo, créer une balise img et y ajouter l'URL de l'image
+              mediaElement = document.createElement("img");
+              mediaElement.classList.add("mediaElement");
+              mediaElement.width = 720;
+              mediaElement.setAttribute(
+                "aria-label",
+                "image de " + media.title
+              );
+              mediaElement.setAttribute("tabindex", "0");
+              mediaElement.src = `assets/photos/${media.image}`;
+              mediaElement.alt = media.title;
+            }
+            const mediaCard = document.createElement("div");
+            mediaCard.classList.add("images");
+            mediaCard.id = media.id;
+
+            mediaCard.appendChild(mediaElement);
+            carrousel.appendChild(mediaCard);
+          });
         }
       });
     });
-    // Ajouter la carte du média à la liste des médias
-    mediaList.appendChild(mediaCard);
-
-    const carrousel = document.querySelector(".carrousel-media");
-    carrousel.innerHTML = "";
-    media.forEach((media) => {
-      let mediaElement;
-      if (media.hasOwnProperty("video")) {
-        // Si le média contient une vidéo, créer une balise vidéo et y ajouter l'URL de la vidéo
-        mediaElement = document.createElement("video");
-        mediaElement.setAttribute("aria-label", "video de " + media.title);
-        mediaElement.classList.add("mediaElement");
-        mediaElement.src = `assets/videos/${media.video}`;
-        mediaElement.controls = true;
-      } else {
-        // Si le média ne contient pas de vidéo, créer une balise img et y ajouter l'URL de l'image
-        mediaElement = document.createElement("img");
-        mediaElement.classList.add("mediaElement");
-        mediaElement.setAttribute("aria-label", "image de " + media.title);
-        mediaElement.setAttribute("tabindex", "0");
-        mediaElement.src = `assets/photos/${media.image}`;
-        mediaElement.alt = media.title;
-      }
-      const mediaCard = document.createElement("div");
-      mediaCard.classList.add("images");
-      mediaCard.id = media.id;
-
-      const imageContainer = document.createElement("div");
-      imageContainer.classList.add("imageContainer");
-
-      mediaCard.appendChild(imageContainer);
-      carrousel.appendChild(mediaCard);
-
-      imageContainer.appendChild(mediaElement);
-
-      //ajouter le titre du média à la carte du média
-      const mediaTitle = document.createElement("h2");
-      mediaTitle.innerHTML = media.title;
-      mediaTitle.classList.add("mediaTitle");
-      mediaCard.appendChild(mediaTitle);
-    });
-
-    // au click sur entrer ouvrir le carrousel
-    document.getElementById(mediaCard.id).addEventListener("keyup", (e) => {
-      if (e.key === "Enter") {
-        // afficher le tableau de média
-        const carrousel = document.querySelector(".carrousel-media");
-        carrousel.innerHTML = "";
-        media.forEach((media) => {
-          let mediaElement;
-          if (media.hasOwnProperty("video")) {
-            // Si le média contient une vidéo, créer une balise vidéo et y ajouter l'URL de la vidéo
-            mediaElement = document.createElement("video");
-            mediaElement.setAttribute("aria-label", "video de " + media.title);
-            mediaElement.classList.add("mediaElement");
-            // add a size to the video to avoid the video to be bigger than the screen
-            mediaElement.width = 720;
-            mediaElement.src = `assets/videos/${media.video}`;
-            mediaElement.controls = true;
-          } else {
-            // Si le média ne contient pas de vidéo, créer une balise img et y ajouter l'URL de l'image
-            mediaElement = document.createElement("img");
-            mediaElement.classList.add("mediaElement");
-            mediaElement.width = 720;
-            mediaElement.setAttribute("aria-label", "image de " + media.title);
-            mediaElement.setAttribute("tabindex", "0");
-            mediaElement.src = `assets/photos/${media.image}`;
-            mediaElement.alt = media.title;
-          }
-          const mediaCard = document.createElement("div");
-          mediaCard.classList.add("images");
-          mediaCard.id = media.id;
-
-          mediaCard.appendChild(mediaElement);
-          carrousel.appendChild(mediaCard);
-        });
-      }
-    });
-  });
   });
 
   const namedForm = document.querySelector(".contact_header");
@@ -234,9 +263,7 @@ async function getPhotographerData() {
   <button aria-label="fermer le formulaire" id="close" onclick="closeModal()">X</button>
   <h3>${photographer[0].name}</h3>
   `;
-
 }
-
 
 getPhotographerData();
 
@@ -253,22 +280,20 @@ function nextImg() {
   document.querySelector(".carrousel-media").scrollLeft += slider;
 }
 
-function displaySlider() {
+
+function displaySlider()  {
   const container = document.querySelector("#carrousel");
   container.style.display = "block";
-  // add aria-hidden to false to the modal
   container.setAttribute("aria-hidden", "false");
   const photograph = document.querySelector("#photographer-main");
   photograph.style.display = "none";
 }
 
-function closeSliderModal(event) {
+
+function closeSliderModal() {
   const container = document.querySelector("#carrousel");
   container.style.display = "none";
   container.setAttribute("aria-hidden", "true");
   const photograph = document.querySelector("#photographer-main");
   photograph.style.display = "block";
 }
-
-
-
